d33993e184c781b83747f1702b4b15ce4b823b48
Jonathan Norman <jnorman2123@gmail.com>
2019-10-25 15:27:46 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,8 +2,7 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize
     @@all << self
   end
 
