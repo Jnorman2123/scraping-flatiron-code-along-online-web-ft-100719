7244f97b5cce8ddfd3f4c05ed5f00bb49127083c
Jonathan Norman <jnorman2123@gmail.com>
2019-10-25 15:58:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,24 +17,24 @@ class Scraper
     # end
   end
 
-  # def get_courses
-  #
-  # end
-  #
-  # def make_courses
-  #
-  # end
-  #
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+  def get_courses
+  
+  end
+  
+  def make_courses
+  
+  end
+  
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 end
 
 Scraper.new.get_page
